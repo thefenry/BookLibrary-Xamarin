5f0296f1b2f99cc4b0eecb50c07513cbede013ee
Ivan Sued <thefenry@gmail.com>
2018-05-22 19:15:59 -0700

    changing the binding logic

--- BookLibrary/BookLibrary/ViewModels/BaseViewModel.cs
+++ BookLibrary/BookLibrary/ViewModels/BaseViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 
 namespace BookLibrary.ViewModels
@@ -11,32 +12,56 @@ namespace BookLibrary.ViewModels
         public bool IsBusy
         {
             get { return isBusy; }
-            set { SetProperty(ref isBusy, value); }
+            set {
+                isBusy = value;
+                RaisePropertyChanged("IsBusy");
+            }
         }
 
-        protected bool SetProperty<T>(ref T backingStore, T value,
-           [CallerMemberName]string propertyName = "",
-           Action onChanged = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(backingStore, value))
-                return false;
+        //protected bool SetProperty<T>(ref T backingStore, T value,
+        //   [CallerMemberName]string propertyName = "",
+        //   Action onChanged = null)
+        //{
+        //    if (EqualityComparer<T>.Default.Equals(backingStore, value))
+        //        return false;
 
-            backingStore = value;
-            onChanged?.Invoke();
-            OnPropertyChanged(propertyName);
-            return true;
-        }
+        //    backingStore = value;
+        //    onChanged?.Invoke();
+        //    OnPropertyChanged(propertyName);
+        //    return true;
+        //}
 
-        #region INotifyPropertyChanged
+        //#region INotifyPropertyChanged
+        //public event PropertyChangedEventHandler PropertyChanged;
+        //protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        //{
+        //    var changed = PropertyChanged;
+        //    if (changed == null)
+        //        return;
+
+        //    changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
+        //#endregion
+
+
+
         public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            var changed = PropertyChanged;
-            if (changed == null)
-                return;
 
-            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-        #endregion
+
+        //public string GetPropertyName<T>(Expression<Func<T>> propertyLambda)
+        //{
+        //    var me = propertyLambda.Body as MemberExpression;
+
+        //    if (me == null)
+        //    {
+        //        throw new ArgumentException("You must pass a lambda of the form: '() => Class.Property' or '() => object.Property'");
+        //    }
+
+        //    return me.Member.Name;
+        //}
     }
 }
