3554c0fb4c275e42f98100f6b03bee10f5be7369
Ivan Sued <thefenry@gmail.com>
2019-05-29 18:27:04 -0700

    Adding Picker to the movie add page

--- BookLibrary/BookLibrary/Views/Movies/AddMovie.xaml.cs
+++ BookLibrary/BookLibrary/Views/Movies/AddMovie.xaml.cs
@@ -30,28 +30,33 @@ namespace BookLibrary.Views.Movies
 
         async void OnScan_Clicked(object sender, EventArgs eventArgs)
         {
-            try
-            {
-                ZXing.Mobile.MobileBarcodeScanner scanner = new ZXing.Mobile.MobileBarcodeScanner();
+            //try
+            //{
+            //    ZXing.Mobile.MobileBarcodeScanner scanner = new ZXing.Mobile.MobileBarcodeScanner();
 
-                ZXing.Result result = await scanner.Scan();
+            //    ZXing.Result result = await scanner.Scan();
 
-                if (result != null)
-                {
-                    Console.WriteLine("Scanned Barcode: " + result.Text);
+            //    if (result != null)
+            //    {
+            //        Console.WriteLine("Scanned Barcode: " + result.Text);
 
-                    //var service = new GoogleBooksService();
-                    //this._movie.Movie = await service.GetBookAsync(result.Text);                    //var service = new GoogleBooksService();
-                    //this._movie.Movie = await service.GetBookAsync(result.Text);
-                }
+            //        //var service = new GoogleBooksService();
+            //        //this._movie.Movie = await service.GetBookAsync(result.Text);                    //var service = new GoogleBooksService();
+            //        //this._movie.Movie = await service.GetBookAsync(result.Text);
+            //    }
 
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("Error Occured", "An Error occured while scanning. Try again later.", "Ok");
-                Log.Warning("Scanner", ex.Message);
-            }
+            //}
+            //catch (Exception ex)
+            //{
+            //    await DisplayAlert("Error Occured", "An Error occured while scanning. Try again later.", "Ok");
+            //    Log.Warning("Scanner", ex.Message);
+            //}
 
         }
+
+        //private void Movie_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        //{
+
+        //}
     }
 }
